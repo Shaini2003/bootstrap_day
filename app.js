2b124842228f1a2b9e0ba20a2c78be3babf84d5f
Shaini2003 <shinitharushika2020@gmail.com>
2024-07-29 11:44:01 +0530

    fifth commit

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Hello World");
+let name = "shaini";
+let age = 20;
+let isActive = true;
+let address = undefined;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isActive);
+console.log(typeof address);
+
+let isEqual = (5 == "5");
+let isThisEqual = (5 === "5");
+
+console.log(isEqual);
+console.log(isThisEqual);
+
+const customer ={
+    name:"Tharu",
+    age:20,
+    phone:"0771234567",
+    city:"Galle"
+}
+console.log(customer.name);
+console.log(customer.phone);
+console.log(customer.city);
